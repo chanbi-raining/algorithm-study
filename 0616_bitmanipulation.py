19f1d465660871582915f90adc252f34eefefcb6
GoEun Yu <40286268+chanbi-raining@users.noreply.github.com>
2019-06-23 03:03:06 +0900

    added #5.3, #5.6, #5.7

--- 0616_bitmanipulation.py
+++ 0616_bitmanipulation.py
@@ -49,3 +49,53 @@ if __name__ == '__main__':
     print(bin2str(0.75))
     print(bin2str(0.72))
 '''
+
+# 5.3
+def longest1(N):
+    N = str(N)
+    length = len(N)
+    cnt = front = maxLen = 0
+    for i in range(len(N)):
+        if N[i] == '1':
+            cnt += 1
+        else:
+            front += 1 if front != 0 else 0
+            maxLen = max(maxLen, front + cnt)
+            front = cnt if i + 1 != length and N[i + 1] == '1' else 0
+            cnt = 0
+    front += 1 if front != 0 else 0
+    maxLen = max(front + cnt, maxLen)
+    return maxLen
+
+'''
+# test code
+if __name__ == '__main__':
+    print(longest1(111111) == 6)
+    print(longest1(1100000111000) == 3)
+    print(longest1(1101110001) == 6)
+    print(longest1(11011101111) == 8)
+    print(longest1(11100001111) == 4)
+    print(longest1(11011100011111111) == 8)
+'''
+
+
+# 5.6
+def change2B(a, b):
+    c = a ^ b
+    count = 0
+    while c != 0:
+        c = c & (c - 1)
+        count += 1
+        print(bin(c))
+    return count
+
+'''
+# test code
+if __name__ = '__main__':
+    print(change2B(29, 15) == 2)
+'''
+
+# 5.7
+def swapBits(N):
+    length = (len(bin(N)[2:]) + 3) // 4
+    return  ((N & int('5' * length, 16)) << 1) + ((N & int('a' * length, 16)) >> 1)    
