b31e9c3cbcb11297dda3d20dffad75f9434b28e0
GoEun Yu <40286268+chanbi-raining@users.noreply.github.com>
2019-07-14 12:13:28 +0900

    added ~ #8.5

--- 0714_memoization.py
+++ 0714_memoization.py
@@ -31,11 +31,55 @@ def gridSearch(r, c, prohibited=[]):
         nextt.append((idx + 1, (pos[0], pos[1] + 1)))
         while True:
             if not nextt:
-                return 'no path possible'
+                return -1
             idx, temp = nextt.pop()
             if temp not in prohibited and temp[0] <= r and temp[1] <= c:
                 pos = temp
-                print(pos)
                 break
     path[-1] = pos
     return path
+
+# 8.3
+def magicidx(lst, dup=False):
+    N = len(lst)
+    idx = 0
+    if idx[-1] < 0: return False
+    while idx < N:
+        if lst[idx] == idx:
+            return idx
+        elif lst[idx] < idx:
+            idx += 1
+        else:
+            if lst[idx] > N - 1: return False 
+            idx = lst[idx] + 1 if not dup else lst[idx]
+    return False
+
+# 8.4
+def partial(lst):
+    N = len(lst) 
+    answer = []
+    if not lst: return [{}]
+    for i in range(2 ** N):
+        answer.append(format(i, '0' + str(N) + 'b'))
+    for idx, j in enumerate(answer):
+        answer[idx] = set(map(lambda x: lst[x] if j[x] == '1' else None, range(N)))
+        answer[idx] = set(filter(lambda x: x != None, answer[idx]))
+    return answer
+
+# 8.5
+def rec_mul(a, b):
+    minn, maxx = min(a, b), max(a, b)
+    mul = format(minn, 'b')
+    answer = 0
+    init = True
+    while mul:
+        if init:
+            temp = maxx
+            init = False
+        else:
+            temp += temp
+        if mul[-1] == '1':
+            answer += temp
+        mul = mul[:-1]
+    return answer
+
